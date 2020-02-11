bf02856954d9f46066f130a4d053d009a8125387
Antonio Yang <yanganto@gmail.com>
2020-02-11 15:51:13 +0800

    Fix ci message color

--- .maintain/ci/darwinia_test_script.sh
+++ .maintain/ci/darwinia_test_script.sh
@@ -7,26 +7,26 @@ source ~/.cargo/env
 case $TARGET in
 	# Format check in stable rust
 	"rustfmt")
-		echo -e "\e[0;32m +------------+ \n | No Test    | \n +------------+ \e[0m"
+		echo -e "\e[0;32m +------------+ \n\e[0;32m  | No Test    | \n\e[0;32m  +------------+ \e[0m"
 		;;
 
 	# Without WASM, build then test
 	"native")
 		SKIP_WASM_BUILD=1 cargo test -p darwinia-kton "$@"
-		echo -e "\e[0;32m +------------+ \n | Kton  Pass | \n +------------+ \e[0m"
+		echo -e "\e[0;32m +------------+ \n\e[0;32m  | Kton  Pass | \n\e[0;32m  +------------+ \e[0m"
 		SKIP_WASM_BUILD=1 cargo test -p darwinia-ring "$@"
-		echo -e "\e[0;32m +------------+ \n | Ring  Pass | \n +------------+ \e[0m"
+		echo -e "\e[0;32m +------------+ \n\e[0;32m  | Ring  Pass | \n\e[0;32m  +------------+ \e[0m"
 		SKIP_WASM_BUILD=1 cargo test -p darwinia-staking "$@"
-		echo -e "\e[0;32m +------------+ \n | Staking OK | \n +------------+ \e[0m"
+		echo -e "\e[0;32m +------------+ \n\e[0;32m  | Staking OK | \n\e[0;32m  +------------+ \e[0m"
 		;;
 
 	# With WASM, build then test
 	"wasm")
 		WASM_BUILD_TYPE=release cargo test -p darwinia-kton "$@"
-		echo -e "\e[0;32m +------------+ \n | Kton  Pass | \n +------------+ \e[0m"
+		echo -e "\e[0;32m +------------+ \n\e[0;32m  | Kton  Pass | \n\e[0;32m  +------------+ \e[0m"
 		WASM_BUILD_TYPE=release cargo test -p darwinia-ring "$@"
-		echo -e "\e[0;32m +------------+ \n | Ring  Pass | \n +------------+ \e[0m"
+		echo -e "\e[0;32m +------------+ \n\e[0;32m  | Ring  Pass | \n\e[0;32m  +------------+ \e[0m"
 		WASM_BUILD_TYPE=release cargo test -p darwinia-staking "$@"
-		echo -e "\e[0;32m +------------+ \n | Staking OK | \n +------------+ \e[0m"
+		echo -e "\e[0;32m +------------+ \n\e[0;32m  | Staking OK | \n\e[0;32m  +------------+ \e[0m"
 		;;
 esac
